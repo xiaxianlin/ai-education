ff33e3050ed4d778c855b212a8bd43ccc1abcc5e
xiaxianlin <dreamtimefly@163.com>
2025-09-26 17:58:22 +0800

    重构

--- server/admin/routes/unit.py
+++ server/admin/routes/unit.py
@@ -1,28 +1,20 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from service.admin.course_unit import CourseUnitService
-from service.admin.knowledge import KnowledgeService
-from service.admin.question import QuestionService
-from store.database import Database
-from schema import (
-    SearchSchema,
-    ResponseSchema,
-    CourseUnitCreateSchema,
-    CourseUnitUpdateSchema,
-)
 
-router = APIRouter(prefix="/course_unit", tags=["课程单元管理"])
+from common.schema import ResponseSchema
+
+router = APIRouter(prefix="/unit", tags=["课程单元管理"])
 
 
 @router.post("/")
-async def create_course_unit(create: CourseUnitCreateSchema, db: AsyncSession = Database):
+async def create_unit(create: CourseUnitCreateSchema, db: AsyncSession = Database):
     """创建课程单元"""
     res = await CourseUnitService.create(db, create)
     return ResponseSchema(data=res)
 
 
 @router.put("/{unit_id}")
-async def update_course_unit(
+async def update_unit(
     unit_id: int, unit_update: CourseUnitUpdateSchema, db: AsyncSession = Database
 ):
     """更新课程单元"""
@@ -31,7 +23,7 @@ async def update_course_unit(
 
 
 @router.delete("/{unit_id}")
-async def delete_course_unit(unit_id: int, db: AsyncSession = Database):
+async def delete_unit(unit_id: int, db: AsyncSession = Database):
     """删除课程单元"""
     await CourseUnitService.delete(db=db, unit_id=unit_id)
     return ResponseSchema()
@@ -44,7 +36,7 @@ async def update_status(id: int, status: int, db: AsyncSession = Database):
 
 
 @router.get("/search")
-async def list_course_units(params: SearchSchema = Depends(), db: AsyncSession = Database):
+async def list_units(params: SearchSchema = Depends(), db: AsyncSession = Database):
     """获取课程单元列表"""
     data = await CourseUnitService.search(db, params)
     return ResponseSchema(data=data)
@@ -53,19 +45,19 @@ async def list_course_units(params: SearchSchema = Depends(), db: AsyncSession =
 @router.get("/{id}/knowledges")
 async def get_knowledges(id: int, db: AsyncSession = Database):
     """获取PDF处理状态"""
-    data = await KnowledgeService.get_by_course_unit(db, id)
+    data = await KnowledgeService.get_by_unit(db, id)
     return ResponseSchema(data=data)
 
 
 @router.get("/{id}/questions")
 async def get_questions(id: int, page: int = 1, size: int = 10, db: AsyncSession = Database):
     """获取PDF处理状态"""
-    data = await QuestionService.get_by_course_unit(db, id, page, size)
+    data = await QuestionService.get_by_unit(db, id, page, size)
     return ResponseSchema(data=data)
 
 
 @router.get("/{unit_id}")
-async def get_course_unit(unit_id: int, db: AsyncSession = Database):
+async def get_unit(unit_id: int, db: AsyncSession = Database):
     """获取单个课程单元"""
     unit = await CourseUnitService.get_by_id(db=db, unit_id=unit_id)
     return ResponseSchema(data=unit)
